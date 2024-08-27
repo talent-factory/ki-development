872a1649c194e988d702284bec3553a8f16b474d
Daniel Senften <daniel.senften@talent-factory.ch>
2024-08-27 16:48:26 +0200

    Aktualisiere Dokumentation und Abhängigkeiten
    - Hinzugefügte detaillierte Anweisungen zur Verwendung von Cog und Replicate.
    - Neue Datei `replicate-exercise.adoc` für Übungsanweisungen hinzugefügt.
    - Neue Datei `replicate-exercise-advanced.adoc` für fortgeschrittene Übungen eingefügt.
    - Anpassung der COg- und Predict-Dateien zur Konsistenz mit den neuen Anweisungen.
    - Aktualisierung der Abhängigkeiten in `requirements.txt`, u.a. zusätzliche Pakete `click`, `cog`, `easyocr` und `torch`.

--- replicate/hello-world/predict.py
+++ replicate/hello-world/predict.py
@@ -3,7 +3,7 @@ from cog import BasePredictor, Input
 
 class Predictor(BasePredictor):
     def setup(self):
-        self.prefix = "hello"
+        self.prefix = "Hallo, "
 
     def predict(self, text: str = Input(description="Text to prefix with 'hello '")) -> str:
         return self.prefix + " " + text
